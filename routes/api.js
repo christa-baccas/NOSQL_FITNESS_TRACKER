db664d854be4c141766bde4365f9eceb860ca051
christa-baccas <christa.baccas@gmail.com>
2021-11-13 12:53:15 -0600

    added get route to get all workouts from db

---
+++ routes/api.js
@@ -0,0 +1,37 @@
+const router = express.Router();
+const express = require("express");
+const path = require("path");
+const Workout = require("../models/workout.js");
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+// router.post("/api/transaction", ({ body }, res) => {
+//   Transaction.create(body)
+//     .then(dbTransaction => {
+//       res.json(dbTransaction);
+//     })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     });
+// });
+
+// router.post("/api/transaction/bulk", ({ body }, res) => {
+//   Transaction.insertMany(body)
+//     .then(dbTransaction => {
+//       res.json(dbTransaction);
+//     })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     });
+// });
+
+
+module.exports = router;
